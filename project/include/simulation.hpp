af1f214d45378d7a11ec53ca807ebef978e1f296
Govawi <76686935+Govawi@users.noreply.github.com>
2021-01-05 18:01:40 +0100

    idee new

---
+++ project/include/simulation.hpp
@@ -0,0 +1,21 @@
+#include "line.hpp"
+
+class Simulation
+{
+private:
+    Line line;
+    //vector<Train> attualmente in circolo
+
+
+public:
+    Simulation();
+
+    //funzioni
+    /*
+    funzione che fa partire i treni                                         -> departure()
+    funzione che aggiorna la posizione dei treni e agisce di conseguenza    -> update()
+
+
+    elimina treni arrivati                                                  -> arrival()
+    */
+} ;
---
+++ project/src/simulation.cpp
@@ -0,0 +1,16 @@
+#include "../include/simulation.hpp"
+
+Simulation::Simulation()
+    :line{}
+{
+    for(int i=0; i<1439; i++)
+    {
+        /*
+        
+        fai partire treno che deve partire
+        ogni treno aggiorna posizione, controlla se deve rallentare,o entare in un deposito, aggiorna stazione successiva se a 20 km
+        elimina treni arrivati
+
+        */
+    }
+}
